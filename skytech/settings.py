66a11da1b660bfcb5b445ef0137d1bac28785990
farooq <farooqaziz1993@gmail.com>
2025-04-25 18:29:26 +0530

    add host in settings.py

--- skytech/settings.py
+++ skytech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-i-b!k*_xbqnl*cxquc2ny2%!00ml472u&#80o)=d^+58g$$6%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "skytechadmin.vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
