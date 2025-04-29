dfd8f4ab8315ef041d7838d61d4fb512aa9f011f
farooq <farooqaziz1993@gmail.com>
2025-04-30 00:31:02 +0530

    live cors origin add

--- skytech/settings.py
+++ skytech/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",          # for local React dev server
     "http://127.0.0.1:8000",
+    "https://skytechbackend.vercel.app",
 ]
 
 ROOT_URLCONF = 'skytech.urls'
